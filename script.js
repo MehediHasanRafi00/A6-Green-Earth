4f23283b5533e325651d3dc5854e9b211eb850c8
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-09-06 23:55:46 +0600

    loadcategory and showcategory with api

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Connect");
+const categoriesContainer = document.getElementById("categoriesContainer");
+
+loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      const categories = data.categories;
+      showCategory(categories);
+    });
+};
+
+showCategory = (category) => {
+  category.forEach((cat) => {
+    
+    categoriesContainer.innerHTML += `
+    <li id="${cat.id}" class=" p-2  font-medium hover:text-white hover:bg-[#15803d] rounded-[4px] list-none">
+    ${cat.category_name}
+    </li>
+    `;
+  });
+};
+
+loadCategory();
